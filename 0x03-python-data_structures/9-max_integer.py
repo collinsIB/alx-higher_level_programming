71a296af5d5fca8693292bed4655ed5333880f1c
collins ibeh <ibehcollins@gmail.com>
2024-03-30 14:22:02 -0700

    update 9-max

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,8 +2,9 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None
-    max_int = my_list[0]
+
+    largest = my_list[0]
     for num in my_list:
-        if num > max_int:
-            max_int = num
-            return max_int
+        if num > largest:
+            largest = num
+            return largest
